d5bc879a2d8f79a8d7be1d1806dd2a8c043170ad
Tiguarces <tadek123tadek@o2.pl>
2022-08-10 22:11:27 +0200

    Update Files from the Local

---
+++ TGbook/src/main/java/pl/Tiguarces/TGbook/TGbookApplication.java
@@ -0,0 +1,13 @@
+package pl.Tiguarces.TGbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TGbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TGbookApplication.class, args);
+	}
+
+}
---
+++ TGbook/src/test/java/pl/Tiguarces/TGbook/TGbookApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.Tiguarces.TGbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TGbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
