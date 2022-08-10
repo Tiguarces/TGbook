a8a57f47b12dc8ffc39a0e61c225a703ba65ed51
Tiguarces <tadek123tadek@o2.pl>
2022-08-10 22:25:01 +0200

    Application file to YAML

--- TGbook/src/main/java/pl/Tiguarces/TGbook/TGbookApplication.java
+++ TGbook/src/main/java/pl/Tiguarces/TGbook/TGbookApplication.java
@@ -1,4 +1,4 @@
-package pl.Tiguarces.TGbook;
+ package pl.Tiguarces.TGbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
